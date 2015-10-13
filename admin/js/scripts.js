c95f5a123acef12a448cfb4827ac812c33a5f2e6
jaedm97 <jaedm97@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-10-13 11:08:41 +0000

    initial upload
    git-svn-id: https://plugins.svn.wordpress.org/wp-poll/trunk@1264788 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/js/scripts.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function($)
+	{
+		$('#wp_poll_for_date').datepicker({
+            dateFormat : 'yy-mm-dd'
+        });
+		
+		
+		/*$(document).on('click', '.wp_poll_linked', function()
+		{
+			var pre_option = $(this).html();
+			$(this).html('no');
+		})
+		*/
+		
+		
+	});
