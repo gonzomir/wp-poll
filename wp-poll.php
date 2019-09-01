6ef2556bea572f9b636ba65b8ee9944cd8f0c19c
jaedm97 <jaedm97@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2019-09-01 07:41:22 +0000

    New version 3.1.9 with addons released
    git-svn-id: https://plugins.svn.wordpress.org/wp-poll/trunk@2149095 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-poll.php
+++ wp-poll.php
@@ -4,7 +4,7 @@
  *
  * Plugin URI: https://www.pluginbazar.com/plugin/wp-poll/
  * Description: It allows user to poll in your website with many awesome features.
- * Version: 3.1.8
+ * Version: 3.1.9
  * Author: Pluginbazar
  * Text Domain: wp-poll
  * Domain Path: /languages/
@@ -17,6 +17,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }  // if direct access
 
+global $wpdb;
+
+define( 'WPP_TABLE_RESULTS', sprintf( '%spoll_results', $wpdb->prefix ) );
+define( 'WPP_PLUGIN_URL', WP_PLUGIN_URL . '/' . plugin_basename( dirname( __FILE__ ) ) . '/' );
+define( 'WPP_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'WPP_PLUGIN_FILE', plugin_basename( __FILE__ ) );
+define( 'WPP_FORUM_URL', 'https://pluginbazar.com/forums/forum/wp-poll' );
+define( 'WPP_CONTACT_URL', 'https://pluginbazar.com/contact/' );
+define( 'WPP_REVIEW_URL', 'https://wordpress.org/support/plugin/wp-poll/reviews/#new-post' );
+
 
 /**
  * Class WPPollManager
@@ -28,7 +38,6 @@ class WPPollManager {
 	 */
 	function __construct() {
 
-		$this->define_constants();
 		$this->load_scripts();
 		$this->define_classes_functions();
 
@@ -138,23 +147,6 @@ class WPPollManager {
 
 		add_action( 'wp_enqueue_scripts', array( $this, 'front_scripts' ) );
 		add_action( 'admin_enqueue_scripts', array( $this, 'admin_scripts' ) );
-	}
-
-
-	/**
-	 * Define Constants
-	 */
-	function define_constants() {
-
-		global $wpdb;
-
-		define( 'WPP_TABLE_RESULTS', sprintf( '%spoll_results', $wpdb->prefix ) );
-		define( 'WPP_PLUGIN_URL', WP_PLUGIN_URL . '/' . plugin_basename( dirname( __FILE__ ) ) . '/' );
-		define( 'WPP_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
-		define( 'WPP_PLUGIN_FILE', plugin_basename( __FILE__ ) );
-		define( 'WPP_FORUM_URL', 'https://pluginbazar.com/forums/forum/wp-poll' );
-		define( 'WPP_CONTACT_URL', 'https://pluginbazar.com/contact/' );
-		define( 'WPP_REVIEW_URL', 'https://wordpress.org/support/plugin/wp-poll/reviews/#new-post' );
 	}
 }
 
